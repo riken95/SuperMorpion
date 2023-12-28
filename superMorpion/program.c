f00e2380410f6aae7928f84ac20c0dee895255bd
riken95 <ebraud95@gmail.com>
2023-12-28 22:36:56 +0100

    changement majeur fonctionnement

--- superMorpion/program.c
+++ superMorpion/program.c
@@ -26,7 +26,7 @@ const uint16_t * restrict list_winning_masks, const int taille_liste)
     
 
     const uint16_t finished_grids = b->blackMask|b->whiteMask|b->equalMask;
-    
+    int coup_joue = 0;
 
     //On parcourt tous les coups possibles
     for(int grille = 0; grille <9; grille ++) if(!is_filled(finished_grids,grille))//Si la grille n'est pas pleine
@@ -34,45 +34,49 @@ const uint16_t * restrict list_winning_masks, const int taille_liste)
         const uint16_t filled_squares = b->listeSubBoards[grille].blackMask|b->listeSubBoards[grille].whiteMask;
 
 
-        if(!(best_eval>MAX_EVAL*b->player && filled_squares == 0ULL))//Evite de chercher dans des carrés vides et de ralentir le programme pour rien
-        for(int square = 0; square<9; square ++) if(!is_filled(filled_squares,square))//Si le carré n'est pas pris
+        if(!(filled_squares == 0ULL))//Evite de chercher dans des carrés vides et de ralentir le programme pour rien
         {
-            play(b,9*grille+square,list_winning_masks,taille_liste,0); //On joue le coup
-            const int eval_coup = min_max_search(alpha,beta,depth-1,b,list_winning_masks,taille_liste); //On évalue la nouvelle position 
-            
-            unplay(b,9*grille+square); //On annule le coup
-            
-            
-
-            if(b->player == 1)//Les blancs jouent
+            if(!coup_joue)
+                coup_joue = 1;
+            for(int square = 0; square<9; square ++) if(!is_filled(filled_squares,square))//Si le carré n'est pas pris
             {
-                //eval = max(eval,eval_coup)
-                if(eval_coup>best_eval)
-                    best_eval = eval_coup;
+                play(b,9*grille+square,list_winning_masks,taille_liste,0); //On joue le coup
+                const int eval_coup = min_max_search(alpha,beta,depth-1,b,list_winning_masks,taille_liste); //On évalue la nouvelle position 
+                
+                unplay(b,9*grille+square); //On annule le coup
+                
+                
 
-                //Elagage alpha beta
-                if(alpha<eval_coup)
-                    alpha = eval_coup;
+                if(b->player == 1)//Les blancs jouent
+                {
+                    //eval = max(eval,eval_coup)
+                    if(eval_coup>best_eval)
+                        best_eval = eval_coup;
 
-                if(beta<=alpha)
-                    return best_eval;
-            }else //Les noirs jouent
-            {
-                //eval = min(eval,eval_coup)
-                if(eval_coup<best_eval)
-                    best_eval = eval_coup;
+                    //Elagage alpha beta
+                    if(alpha<eval_coup)
+                        alpha = eval_coup;
+
+                    if(beta<=alpha)
+                        return best_eval;
+                }else //Les noirs jouent
+                {
+                    //eval = min(eval,eval_coup)
+                    if(eval_coup<best_eval)
+                        best_eval = eval_coup;
 
 
-                //Elagage alpha beta
-                if(beta > eval_coup)
-                    beta = eval_coup;
+                    //Elagage alpha beta
+                    if(beta > eval_coup)
+                        beta = eval_coup;
 
-                if(beta <=alpha)
-                    return best_eval;
+                    if(beta <=alpha)
+                        return best_eval;
+                }
             }
         }
     }
-    if(best_eval == -MAX_EVAL*b->player)    //Aucun coup n'a été joué : cela signifie que la grille est finie
+    if(!coup_joue)    //Aucun coup n'a été joué : cela signifie qu'aucune grille est partiellement remplie
         return eval(b);
 
     return best_eval;
@@ -87,22 +91,33 @@ int computer_play(board * restrict b,const int max_depth, const uint16_t * restr
     int best_eval = -MAX_EVAL;
     int best_move = -1;
     const int player = b->player;
+    int coup_joue = 0;
+    int grille_default = 0;
     for(int grille = 0; grille<9;grille++) if(!is_filled(finished_grids,grille))//Si la grille n'est pas pleine
     {
         uint16_t filled_squares = b->listeSubBoards[grille].blackMask|b->listeSubBoards[grille].whiteMask;
 
-        if(!(best_eval>MAX_EVAL*b->player && filled_squares == 0ULL))//Evite de chercher dans des carrés vides et de ralentir le programme pour rien
-        for(int square = 0; square<9; square ++) if(!is_filled(filled_squares,square))//Si le carré n'est pas pris
+        if(!filled_squares == 0ULL)//Evite de chercher dans des carrés vides et de ralentir le programme pour rien
         {
-            play(b,9*grille+square,list_winning_masks,taille_liste,0); //On joue le coup
-            const int eval_coup = min_max_search(-MAX_EVAL,MAX_EVAL,max_depth,b,list_winning_masks,taille_liste); //On évalue la nouvelle position 
-            unplay(b,9*grille+square); //On annule le coup
-            if(eval_coup *player > best_eval)
+            if(!coup_joue)
+                coup_joue =1;
+            for(int square = 0; square<9; square ++) if(!is_filled(filled_squares,square))//Si le carré n'est pas pris
             {
-                best_eval = eval_coup * player;
-                best_move = 9*grille + square;
+                play(b,9*grille+square,list_winning_masks,taille_liste,0); //On joue le coup
+                const int eval_coup = min_max_search(-MAX_EVAL,MAX_EVAL,max_depth,b,list_winning_masks,taille_liste); //On évalue la nouvelle position 
+                unplay(b,9*grille+square); //On annule le coup
+                if(eval_coup *player > best_eval)
+                {
+                    best_eval = eval_coup * player;
+                    best_move = 9*grille + square;
+                }
             }
+        }else
+        {
+            grille_default = grille;
         }
     }
+    if(!coup_joue) //Aucun coup dans une grille déjà remplie n'était jouable. On renvoie 0 indiquant de jouer le coup du milieu dans une grille neutre
+        return grille_default*9+5;
     return best_move;
 }
